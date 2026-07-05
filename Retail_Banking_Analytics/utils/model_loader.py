ca3d18997dcc7655e0c708af5789902916accd36
Manu123778 <manu9390992647@gmail.com>
2026-07-06 00:08:13 +0530

    Update model_loader.py

--- Retail_Banking_Analytics/utils/model_loader.py
+++ Retail_Banking_Analytics/utils/model_loader.py
@@ -1,12 +1,14 @@
+from pathlib import Path
 import joblib
 import streamlit as st
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+MODEL_DIR = BASE_DIR / "models"
 
-@st.cache_resource
+@st.cache_data
 def load_model():
 
-    model = joblib.load("models/loan_model.pkl")
-
-    scaler = joblib.load("models/scaler.pkl")
+    model = joblib.load(MODEL_DIR / "loan_model.pkl")
+    scaler = joblib.load(MODEL_DIR / "scaler.pkl")
 
     return model, scaler
