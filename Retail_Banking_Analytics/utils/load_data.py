7e61326c120dbcf9701a732e35dae9dc20b7d3ac
Manu123778 <manu9390992647@gmail.com>
2026-07-06 00:04:36 +0530

    Update load_data function to use dynamic paths
    Refactor data loading to use BASE_DIR and DATA_DIR for file paths.

--- Retail_Banking_Analytics/utils/load_data.py
+++ Retail_Banking_Analytics/utils/load_data.py
@@ -1,46 +1,30 @@
-# Import libraries
-
+from pathlib import Path
 import pandas as pd
 import streamlit as st
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = BASE_DIR / "data"
 
-# Load all datasets
 
 @st.cache_data
 def load_data():
 
-    account = pd.read_csv("data/account.csv")
-
-    client = pd.read_csv("data/client.csv")
-
-    district = pd.read_csv("data/district.csv")
-
-    disposition = pd.read_csv("data/disposition.csv")
-
-    card = pd.read_csv("data/card.csv")
-
-    loan = pd.read_csv("data/loan.csv")
-
-    orders = pd.read_csv("data/orders.csv")
-
-    transactions = pd.read_csv("data/transactions.csv")
+    account = pd.read_csv(DATA_DIR / "account.csv")
+    client = pd.read_csv(DATA_DIR / "client.csv")
+    district = pd.read_csv(DATA_DIR / "district.csv")
+    disposition = pd.read_csv(DATA_DIR / "disposition.csv")
+    card = pd.read_csv(DATA_DIR / "card.csv")
+    loan = pd.read_csv(DATA_DIR / "loan.csv")
+    orders = pd.read_csv(DATA_DIR / "orders.csv")
+    transactions = pd.read_csv(DATA_DIR / "transactions.csv")
 
     return {
-
         "account": account,
-
         "client": client,
-
         "district": district,
-
         "disposition": disposition,
-
         "card": card,
-
         "loan": loan,
-
         "orders": orders,
-
-        "transactions": transactions
-
+        "transactions": transactions,
     }
